31245fb6440e30b76f2f2e14828112ce934221d3
Positron Games <sharrvathgopi@gmail.com>
2025-05-27 17:52:07 +0530

    Update Game.h

--- Game.h
+++ Game.h
@@ -10,6 +10,7 @@
 #include "Shader.h"
 #include "Texture.h"
 #include "Dep/include/stb_image.h"
+#include "Player.h"
 
 class Game
 {
@@ -19,28 +20,19 @@ public:
 
 	Game(std::string _name);
 
-	Shader red_box_shader;
-	//Shader green_box_shader;
-	//Shader blue_box_shader;
+	Shader def_shader;
 
 	Texture red_box;
-	unsigned int red_box_texture;
-	//Texture green_box;
-	//Texture blue_box;
+	Texture green_box;
+	Texture blue_box;
 
-	Shader def_shader;
-	
-	float vertices[20];
-	float indices[6];
+	Player player;
 
-	unsigned int vao;
-	unsigned int vbo;
-	unsigned int ebo;
 
 	void Init();
-	void Update();
+	void Update(float delta, GLFWwindow* g_window);
 	void Draw();
-	void Run();
+	void Run(float delta, GLFWwindow* _window);
 	void CleanUp();
 
 };
