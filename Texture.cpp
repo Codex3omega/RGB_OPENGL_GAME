a50c7c3278943129658d0d0e3cdbcae04597f416
Positron Games <sharrvathgopi@gmail.com>
2025-05-27 17:52:52 +0530

    Update Texture.cpp

--- Texture.cpp
+++ Texture.cpp
@@ -1,53 +1,7 @@
 #include "Texture.h"
 
 
-bool Texture::create(float normal_w, float normal_h, const char* _path, unsigned int _id, Shader& shader) {
-
-	float _vertices [20] = {
-		
-		-normal_w, normal_h, 0.0f, 0.0f, 1.0f, // Top left vertex
-		normal_w, normal_h, 0.0f, 1.0f, 1.0f, // Top Right vertex
-		normal_w, -normal_h, 0.0f, 1.0f, 0.0f, // Bottom right vertex
-		-normal_w, -normal_h, 0.0f, 0.0f, 0.0f // Bottom left vertex
-
-	
-	};
-
-	unsigned int _indices[6] = {
-	
-		0, 1, 2,
-		2, 3, 1
-	
-	};
-
-	id = _id;
-
-	// store 
-	std::copy(_vertices, _vertices + 20, vertices);
-	std::copy(_indices, _indices + 6, indices);
-
-	std::cout << vertices[1] << std::endl;
-
-
-	stbi_set_flip_vertically_on_load(true);
-
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
-
-	glGenBuffers(1, &vbo);
-	glGenBuffers(1, &ebo);
-
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
+bool Texture::init(float normal_w, float normal_h, const char* _path, unsigned int _id) {
 
 	glGenTextures(1, &texture);
 	glBindTexture(GL_TEXTURE_2D, texture);
@@ -69,63 +23,23 @@ bool Texture::create(float normal_w, float normal_h, const char* _path, unsigned
 	else {
 
 		std::cout << "Failed to Load Texture" << std::endl;
-
+		return false;
 	}
 	stbi_image_free(data);
 
-	// shader
-	shader.use();
-	shader.setInt("texture_", id);
 	glBindTexture(GL_TEXTURE_2D, 0);
 
-	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-	//glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 	return true;
 }
 
-void Texture::draw(Shader &shader) {
-
-	switch (id) {
-
-	case 0 :
-		glActiveTexture(GL_TEXTURE0);
-		break;
-
-	case 1:
-		glActiveTexture(GL_TEXTURE1);
-		break;
-
-	case 2:
-		glActiveTexture(GL_TEXTURE2);
-		break;
-
-	case 3:
-		glActiveTexture(GL_TEXTURE3);
-		break;
-
-	case 4:
-		glActiveTexture(GL_TEXTURE4);
-		break;
-
-	case 5:
-		glActiveTexture(GL_TEXTURE5);
-		break;
-	}
-
-	glBindTexture(GL_TEXTURE_2D, texture);
-
-	shader.use();
-	glBindVertexArray(vao);
-	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-	glBindVertexArray(0);
-	
-}
-
 void Texture::UnloadTexture() {
 
-	glDeleteVertexArrays(1, &vao);
-	glDeleteBuffers(1, &vbo);
-	glDeleteBuffers(1, &ebo);
+	glDeleteTextures(1, &texture);
 
+}
+
+void Texture::activate_texture() {
+	glActiveTexture(GL_TEXTURE0 + id);
+	glBindBuffer(GL_TEXTURE_2D, texture);
 }
