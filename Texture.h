ce52e44b2d5cb88c635c771fa18494a2562252f9
Positron Games <sharrvathgopi@gmail.com>
2025-05-27 17:53:13 +0530

    Update Texture.h

--- Texture.h
+++ Texture.h
@@ -1,37 +1,29 @@
 #pragma once
 #include <glad/glad.h>
 #include <stb_image.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <string>
 #include <algorithm>
 #include <iostream>
 #include "Shader.h"
-
 
 class Texture
 {
 
 public:
 
-
-
-	float vertices[20];
-	float indices[6];
-
-
 	const char* path;
-
-	unsigned int vao;
-	unsigned int vbo;
-	unsigned int ebo;
-
 	unsigned int id;
 	unsigned int texture;
 
+	Shader* shader;
+
 	std::string shader_texture_name;
 	
-
-	bool create(float normalized_width, float normalized_height, const char* _path, unsigned int _id, Shader& shader);
-	void draw(Shader &shader);
+	bool init(float normalized_width, float normalized_height, const char* _path, unsigned int _id);
+	void activate_texture();
 	void UnloadTexture();
 
 
